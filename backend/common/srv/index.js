fe89d3f8628117c472c87cd6ad3d90d0b5f4ebd2
Theo <theostanton@gmail.com>
2020-01-23 19:38:58 +0000

    rm srv contents

--- backend/common/srv/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var schema_1 = __importDefault(require("./schema"));
-exports.schema = schema_1.default;
-var Logger_1 = require("./Logger");
-exports.Logger = Logger_1.Logger;
