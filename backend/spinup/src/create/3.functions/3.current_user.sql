c687f377888d1ac318a42b032a02a5edda1c3329
Theo <theostanton@gmail.com>
2020-02-14 17:45:42 +0100

    cast for get_current_user

--- backend/spinup/src/create/3.functions/3.current_user.sql
+++ backend/spinup/src/create/3.functions/3.current_user.sql
@@ -3,7 +3,7 @@ create function "get_current_user"() returns jwt_token
     language sql
 as
 $$
-select 'guided_user'                                   as role,
-       current_setting('jwt.claims.username', true)    as username,
-       current_setting('jwt.claims.exp', true)::bigint as exp
+select 'guided_user' ::text                               as role,
+       current_setting('jwt.claims.username', true)::text as username,
+       current_setting('jwt.claims.exp', true)::bigint    as exp
 $$;
