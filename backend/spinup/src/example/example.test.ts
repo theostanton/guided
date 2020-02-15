006406a74f4ee25ca57ffe9cab5d5a8482e40682
Theo <theostanton@gmail.com>
2020-02-15 18:01:07 +0100

    skip tests :)

--- backend/spinup/src/example/example.test.ts
+++ backend/spinup/src/example/example.test.ts
@@ -1,15 +1,17 @@
+import execute from "../."
 import { populateTables } from "../tests"
+import { database } from "@guided/database"
 
 
 beforeEach(async () => {
   await populateTables()
 })
-test("Simple add 1 user", async () => {
-  // const usersBefore = await database.manyOrNone(`SELECT *
-  //                                                from users`)
-  // expect(usersBefore.length).toBe(1)
-  // await execute()
-  // const usersAfter = await database.manyOrNone(`SELECT *
-  //                                               from users`)
-  // expect(usersAfter.length).toBe(2)
+xtest("Simple add 1 user", async () => {
+  const usersBefore = await database.manyOrNone(`SELECT *
+                                                 from users`)
+  expect(usersBefore.length).toBe(1)
+  await execute()
+  const usersAfter = await database.manyOrNone(`SELECT *
+                                                from users`)
+  expect(usersAfter.length).toBe(2)
 })
