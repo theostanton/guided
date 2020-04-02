4c73cea3bec1272c0fa45d33bb63d78b73323d4b
Theo <theostanton@gmail.com>
2020-04-02 14:53:42 +0200

    install terraform

---
+++ deploy/install_terraform.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+CIRCLECI_CACHE_DIR="/usr/local/bin"
+TERRAFORM_VERSION="0.12.10"
+TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+
+if [ ! -f "${CIRCLECI_CACHE_DIR}/terraform" ] || [[ ! "$(terraform version)" =~ "Terraform v${PACKER_VERSION}" ]]; then
+  wget -O /tmp/terraform.zip "${TERRAFORM_URL}"
+  unzip -oud "${CIRCLECI_CACHE_DIR}" /tmp/terraform.zip
+fi
+
+terraform version
