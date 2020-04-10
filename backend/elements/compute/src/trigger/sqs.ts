2dbe42047dfc207501b2e0d9afa35700b44f6850
Theo <theostanton@gmail.com>
2020-04-10 21:58:40 +0200

    creds

--- backend/elements/compute/src/trigger/sqs.ts
+++ backend/elements/compute/src/trigger/sqs.ts
@@ -1,14 +1,9 @@
 import AWS from "aws-sdk"
 
-
-if (!process.env.AWS_ACCESS_KEY_ID) {
-  throw new Error("No process.env.AWS_ACCESS_KEY_ID for SQS")
-}
-
 export default new AWS.SQS({
   region: "eu-west-2",
-  credentials: {
+  credentials: process.env.AWS_ACCESS_KEY_ID ? {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
+  } : undefined,
 })
