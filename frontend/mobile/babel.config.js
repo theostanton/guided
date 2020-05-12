d9933906d88e8a4b39f1d2bbfde14757dba2b959
Theo <theostanton@gmail.com>
2020-05-12 09:31:20 +0200

    babel module config

--- frontend/mobile/babel.config.js
+++ frontend/mobile/babel.config.js
@@ -1,9 +1,19 @@
 module.exports = function(api) {
   api.cache(true)
   return {
-    // plugins: [
-    //   "@babel/plugin-proposal-decorators", { "legacy": true },
-    // ],
+    "plugins": [
+      [
+        "module-resolver",
+        {
+          "root": ["."],
+          "alias": {
+            "components": "./src/components",
+            "api": "./src/api",
+            "model": "./src/model",
+          },
+        },
+      ],
+    ],
     presets: [
       "babel-preset-expo",
       // "module:metro-react-native-babel-preset",
