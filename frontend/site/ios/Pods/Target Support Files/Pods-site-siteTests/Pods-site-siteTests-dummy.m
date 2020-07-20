21edc990d819a188e9fee0fd5f2babfbac3424a0
Theo <theostanton@gmail.com>
2020-07-20 12:23:48 +0200

    rm pods

--- frontend/site/ios/Pods/Target Support Files/Pods-site-siteTests/Pods-site-siteTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_site_siteTests : NSObject
-@end
-@implementation PodsDummy_Pods_site_siteTests
-@end
--- frontend/site/ios/Pods/Target Support Files/Pods-site-tvOSTests/Pods-site-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_site_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_site_tvOSTests
-@end
