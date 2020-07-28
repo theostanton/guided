937a70c2c1d137b615c0c0cb823d6853109eefac
Theo <theostanton@gmail.com>
2020-07-28 14:10:56 +0200

    .next

--- frontend/site/.next/static/development/_buildManifest.js
+++ frontend/site/.next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/signup":["static\u002Fchunks\u002Fpages\u002Fsignup.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
